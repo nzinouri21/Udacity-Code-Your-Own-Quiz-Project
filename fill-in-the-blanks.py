bbc26dd8370533d03dd1ee97a43ca8cb579bebce
Nazanin <n.zinouri@gmail.com>
2017-12-07 19:55:08 -0500

    difficulty level
    defining a function that pops out the question and answers based on the difficulty level

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -1,50 +1,12 @@
-# IPND Stage 2 Final Project
-
-# You've built a Mad-Libs game with some help from Sean.
-# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
-
-# For this project, you'll be building a Fill-in-the-Blanks quiz.
-# Your quiz will prompt a user with a paragraph containing several blanks.
-# The user should then be asked to fill in each blank appropriately to complete the paragraph.
-# This can be used as a study tool to help you remember important vocabulary!
-
-# Note: Your game will have to accept user input so, like the Mad Libs generator,
-# you won't be able to run it using Sublime's `Build` feature.
-# Instead you'll need to run the program in Terminal or IDLE.
-# Refer to Work Session 5 if you need a refresher on how to do this.
-
-# To help you get started, we've provided a sample paragraph that you can use when testing your code.
-# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
-
-sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
-
-# The answer for ___1___ is 'function'. Can you figure out the others?
-
-# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
-# A .pyc file is a Python file that has been translated into "byte code".
-# This means the code will run the same as the original .py file, but when you open it
-# it won't look like Python code! But you can run it just like a regular Python file
-# to see how your code should behave.
-
-# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
-# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
-# How can you adapt that design to work with numbered blanks?
-
-# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd-1-1/20min/
-
-
-
-##################################################################################3
+###############################3
 #The following are the lists containing the words that are the correct answers
 #to each level of the game
 answers_level_easy  = ["Howard", "Caltech", "The Cheesecake Factory", "India"]
 answers_level_medium  = ["Barenaked Ladies", "fourteen billion", "Sheldon", "Soft Kitty"]
 answers_level_difficult  = ["Stan Lee", "Friday", "Monday", "Laundry"]
 
-
+#The following are sentences containing the missing words that the player should
+#guess the words
 question_level_easy = '''Out of the four guys,  ___1___ is the only one who doesn't have a PhD.
 Sheldon and Leonard both work at  ___2___ . In the first few seasons, Penny works at  ___3___ .
 Rajesh is originally from ___4___ .'''
@@ -58,3 +20,30 @@ question_level_difficult = '''Sheldon lost the opportunity to meet ___1___ when 
 to jail after insulting a judge in traffic court. According to the "Sheldonian Calendar",
 ___2___ night is "Vintage Video Game Night", ___3___ night is "Thai Takeout" night and
 Saturday night is of course ___4___ night.'''
+
+available_levels = ["easy", "medium", "difficult"]
+
+#This function asks the user to choose a difficulty level.
+#If the difficulty level is not in the available_levels, the player will receive an error
+#The purpose of this function is to decide which question and which answers should
+#be used to run the game
+def choose_difficulty_level(level):
+    if level in available_levels:
+        if level == "easy":
+            answers = answers_level_easy
+            question = question_level_easy
+            return "You chose easy level!"
+        elif level == "medium":
+            answers = answers_level_medium
+            question = question_level_medium
+            return "You chose medium level!"
+        elif level == "medium":
+            answers = answers_level_difficult
+            question = question_level_difficult
+            return "You chose difficult level!"
+    else:
+        return "Sorry, you selected the wrong level!"
+
+#Testing choose_difficulty_level function
+#  print choose_difficulty_level("easy")
+#  print choose_difficulty_level("m")
