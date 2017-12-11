f795b63db16960bb66b20f3ff002fb4936756968
Nazanin <n.zinouri@gmail.com>
2017-12-11 16:42:11 -0500

    revision

---
+++ fill-in-the-blanks.py
@@ -0,0 +1,6 @@
+#in-line comment! Starting the game by asking the player's name
+#and greeting them!
+player_name = raw_input("What is your name?")
+print '\n'
+print "Hello " + player_name + "!"
+print '\n'
